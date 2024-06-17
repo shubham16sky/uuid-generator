9f187721629ffbfbd0316c7b8adc9a5e8d40553d
pranit.kumar <pranit.kumar@1digitalstack.ai>
2024-06-17 20:48:49 +0530

    Made few changes and implemented rolling update

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app  = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'messsage':'Hey! It is up and working !'}
+    return {'messsage':'Hey! It is up and working. This is v3 of the application'}
 
 @app.get('/generate')
 async def generate():
